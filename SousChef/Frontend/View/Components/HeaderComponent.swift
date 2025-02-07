7bf189a03b7724d964c33e7437c287e89da0ddf1
oliverzolan <oliverzolan@gmail.com>
2025-02-06 23:51:16 -0800

    uodated pantry ui

---
+++ SousChef/Frontend/View/Components/HeaderComponent.swift
@@ -0,0 +1,8 @@
+//
+//  HeaderComponent.swift
+//  SousChef
+//
+//  Created by Garry Gomes on 2/1/25.
+//
+
+import Foundation
---
+++ SousChef/Frontend/View/Components/navBarComponent.swift
@@ -0,0 +1,8 @@
+//
+//  navBarComponent.swift
+//  SousChef
+//
+//  Created by Garry Gomes on 2/6/25.
+//
+
+import Foundation
