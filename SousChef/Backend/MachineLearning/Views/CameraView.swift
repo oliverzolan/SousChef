64ef881a92dc88375c608097837d78f036498e12
oliverzolan <oliverzolan@gmail.com>
2025-03-08 12:59:06 -0800

    Updated Camera view

--- SousChef/Backend/MachineLearning/Views/CameraView.swift
+++ SousChef/Backend/MachineLearning/Views/CameraView.swift
@@ -6,12 +6,42 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct CameraView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> CameraViewController {
-        return CameraViewController()
+    @Binding var selectedImage: UIImage?
+    @Binding var isPresented: Bool
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: CameraView
+
+        init(parent: CameraView) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.isPresented = false
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.isPresented = false
+        }
     }
 
-    func updateUIViewController(_ uiViewController: CameraViewController, context: Context) {
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
     }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        picker.allowsEditing = false
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
 }
